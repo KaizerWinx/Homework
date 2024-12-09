8634f10a2373a74a315a89e29774140261117664
кирилл кузнецов <saundwave123@yandex.ru>
2024-12-09 11:55:10 +0300

    add .flake8

---
+++ src/masks.py
@@ -0,0 +1,9 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскирует номер карты, заменяя цифры на '*'"""
+    return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+
+
+# Реализация второй функции
+def get_mask_account(account_number: str) -> str:
+    """Функция маскирует номер аккаунта, заменяя цифры на '*'"""
+    return "**" + account_number[-4:]
