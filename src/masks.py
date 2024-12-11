c5f0ee2c89cf49def75ba7f4bf86644070e23d4a
кирилл кузнецов <saundwave123@yandex.ru>
2024-12-11 16:42:57 +0300

    modified src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,9 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номер карты, заменяя цифры на '*'"""
-    return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+    return card_number[-16:-12] + " " + card_number[-12:-10] + "** **** " + card_number[-4:]
 
 
 # Реализация второй функции
 def get_mask_account(account_number: str) -> str:
     """Функция маскирует номер аккаунта, заменяя цифры на '*'"""
-    return "**" + account_number[-4:]
+    return "**" + account_number[-4:] + " ** " + account_number[-4:]
