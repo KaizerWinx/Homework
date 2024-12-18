36089d997cd15ea60396fa5060c23b6e8ea2e6a7
кирилл кузнецов <saundwave123@yandex.ru>
2024-12-18 11:57:22 +0300

    "add pricessing in main"

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_card_number, get_mask_account
 from src.widget import mask_account_card, get_date
-
+from src.processing import filter_by_state, sort_by_date
 
 # Вывод функций
 if __name__ == "__main__":
