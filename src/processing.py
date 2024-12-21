7e0ca07e8a88a38c8243c9c06c8a72d90b32e48f
кирилл кузнецов <saundwave123@yandex.ru>
2024-12-21 16:42:58 +0300

    "fix functions"

--- src/processing.py
+++ src/processing.py
@@ -5,10 +5,11 @@ def filter_by_state(list_of_dicts: list, state_for_key: str = "EXECUTED") -> lis
     for state in list_of_dicts:
         if state.get("state") == state_for_key:
             new_list.append(state)
-            return new_list
+    return new_list
+
 
 def sort_by_date(list_of_dicts: list, ascending: bool = True) -> list:
     """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию - убывание) и возвращает новый список отсортированный по дате"""
-    new_list = [sorted(list_of_dicts, key=lambda date: date.get("date", 0), reverse=ascending)]
+    new_list = sorted(list_of_dicts, key=lambda date: date.get("date", ''), reverse=ascending)
     return new_list
